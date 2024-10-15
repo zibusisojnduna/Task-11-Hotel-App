6528e53dead75abd73967c9ad9fb54f8324c1d3d
User <User@WRSD-LD309-026.dut.ac.za>
2024-10-15 14:11:09 +0200

    feat:redux toolkit added

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,18 +4,15 @@ import { LuCheckCircle } from "react-icons/lu";
 import { TfiCheckBox } from "react-icons/tfi";
 import { IoMdPerson } from "react-icons/io";
 import { IoIosCart } from "react-icons/io";
-
+import "../assets/w3.css"
 function Navigation(){
     return(
-        <div className="w3-bar" style={{backgroundColor:"#a8bfe6"}}>
-        <a></a>
-        <a href="/" class="w3-bar-item w3-button"><LuHome/>Home</a>
-        <a href="" className="w3-bar-item w3-button"><IoBed/>Accommodation</a>
-        <a href="" className="w3-bar-item w3-button"><LuCheckCircle/>Login</a>
-        <a href="" className="w3-bar-item w3-button"><TfiCheckBox/>Sign Up</a>
-        <a href="/" class="w3-bar-item w3-button"><IoMdPerson/>Profile</a>
-        <a href="/" class="w3-bar-item w3-button"><IoIosCart/>Cart</a>
-      </div>
+      <div className="w3-bar w3-black" style={{}}>
+      <a href="#" className="w3-bar-item w3-button w3-mobile">Home</a>
+      <a href="#" className="w3-bar-item w3-button w3-mobile">Accommodation</a>
+      <a href="#" className="w3-bar-item w3-button w3-mobile">Login</a>
+      <a href="#" className="w3-bar-item w3-button w3-mobile">Sign Up</a>
+    </div>
     )
 }
 export default Navigation
